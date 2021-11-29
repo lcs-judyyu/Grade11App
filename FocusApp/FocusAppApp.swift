ea485f82ca36ffe9190395a32f184ac6999410ec
Judy <jyu@lcs.on.ca>
2021-11-29 14:26:23 -0500

    Initial Commit

---
+++ FocusApp/FocusAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusAppApp.swift
+//  FocusApp
+//
+//  Created by Judy Yu on 2021-11-29.
+//
+
+import SwiftUI
+
+@main
+struct FocusAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
