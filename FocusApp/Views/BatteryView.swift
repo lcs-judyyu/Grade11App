8060f8906488816d401a3804081dad919795661a
Judy <jyu@lcs.on.ca>
2021-11-29 19:56:44 -0500

    Add images to the asset, add the information section

--- FocusApp/Views/BatteryView.swift
+++ FocusApp/Views/BatteryView.swift
@@ -18,30 +18,68 @@ struct ContentView: View {
         VStack {
             
             ScrollView {
-                VStack {
-                    Picker(selection: $batteryState,
-                           label: Text("Picker Name"),
-                           content: {
-
-
-                    })
-                        .pickerStyle(WheelPickerStyle())
-                    
-                    Text("About the battery")
+                ZStack {
+                    Color.orange.opacity(0.2)
                     VStack {
-                    Image("charge")
-                            .resizable()
-                            .scaledToFit()
-                            .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
-                            .padding(10)
-                    
+                        Picker(selection: $batteryState,
+                               label: Text("Picker Name"),
+                               content: {
+
+
+                        })
+                            .pickerStyle(WheelPickerStyle())
+                        
+                        //information section
+                        HStack {
+                            VStack(alignment: .leading){
+                                Text("About the battery".capitalized(with: .current))
+                                    .bold()
+                                    .italic()
+                                    .font(.title2)
+                                    .padding(.leading, 20)
+                            }
+                            Spacer()
+                        }
+                        
+                        //first article
+                        VStack(alignment: .leading){
+                        Image("Battery")
+                                .resizable()
+                                .scaledToFit()
+                                .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
+                                .padding(.horizontal, 20)
+                        Text("Battery and Performance")
+                                .bold()
+                                .padding(.leading, 20)
+                                .font(.title3)
+                        
+                        }
+                        .padding(.bottom, 15)
+                        .background(Color.white)
+                        .padding(.bottom, 30)
+                        
+                        //second article
+                        VStack(alignment: .leading){
+                        Image("Charge")
+                                .resizable()
+                                .scaledToFit()
+                                .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
+                                .padding(.horizontal, 20)
+                        Text("General Performance Tips")
+                                .bold()
+                                .padding(.leading, 20)
+                                .font(.title3)
+                        
+                        }
+                        .padding(.bottom, 15)
+                        .background(Color.white)
+                        .padding(.bottom, 30)
                     }
                 }
             }
             
             //fixed footer
             VStack {
-                Spacer()
                 Divider()
                     .padding(.bottom, 10)
                     .frame(height: 10)
@@ -63,6 +101,8 @@ struct ContentView: View {
                         .foregroundColor(Color.teal)
                 }
             }
+            .background(Color.yellow.opacity(0.3))
+            .edgesIgnoringSafeArea(.bottom)
         }
     }
 }
